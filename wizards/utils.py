e5a1f1afdf8b701a27fe53dcb77c51972f227afb
Ben Smith <djbinnz@Bens-MBP.fritz.box>
2017-08-05 18:55:29 +0200

    Added doc string to random partition algorithm.

--- wizards/utils.py
+++ wizards/utils.py
@@ -2,12 +2,29 @@
 from random import randint
 
 
-def random_partition(v, parts):
-    res = {}
-    for p in parts[1:][::-1]:
-        m = v/p
-        res[p] = randint(0, m)
-        v -= p*res[p]
-    res[1] = v
-    return res
+def random_partition(n, parts):
+    """
+    :param n: int
+    :param parts: list
+    :return: dict
 
+    This algorithm takes an integer, n, and a list, parts, of integers and
+    returns a dictionary of key, values (k,v) correspond to an integer
+    partition of n with v copies of k.
+
+    Example: if (n, parts) = (5, [1,2,3]) a possible output could be any of
+    {1: 0, 2: 1,  3: 1},
+    {1: 2, 2: 0, 3: 1},
+    {1: 1, 2: 2, 3: 0},
+    {1: 3, 2: 1, 3: 0},
+    {1: 5, 2: 0, 3: 0}
+
+    with (I think) equal probability.
+    """
+    res = {}
+    for p in reversed(parts[1:]):
+        res[p] = randint(0, n/p)
+        n -= p * res[p]
+
+    res[1] = n
+    return res
